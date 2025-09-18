d4ab30a89ad341fd0d9a15361635f7c447fb67cb
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2025-09-18 14:17:23 +0100

    Add time based panda changes

--- runner.py
+++ runner.py
@@ -1,5 +1,5 @@
 from bluesky import RunEngine
 
-from scripts.plans import no_panda, panda_scan
+from scripts.plans import no_panda, panda_scan, test_store_settings, test_restore_settings, panda_scan_time_based
 
 RE = RunEngine()
--- scripts/plans.py
+++ scripts/plans.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import numpy as np
+import h5py
 import bluesky.plan_stubs as bps
 import bluesky.preprocessors as bpp
 from dodal.common.beamlines.beamline_utils import (
@@ -7,6 +8,7 @@ from dodal.common.beamlines.beamline_utils import (
     get_path_provider,
     set_path_provider,
 )
+from datetime import datetime
 from dodal.common.visit import (
     LocalDirectoryServiceClient,
     StaticVisitPathProvider,
@@ -15,7 +17,8 @@ from dodal.plan_stubs.data_session import attach_data_session_metadata_decorator
 from dodal.utils import BeamlinePrefix, get_beamline_name
 from scanspec.specs import Fly, Line
 
-from ophyd_async.core import DetectorTrigger, StandardFlyer, TriggerInfo
+from ophyd_async.core import DetectorTrigger, StandardFlyer, TriggerInfo, YamlSettingsProvider
+from ophyd_async.plan_stubs import store_settings, retrieve_settings, apply_panda_settings
 from ophyd_async.epics.motor import Motor
 from ophyd_async.epics.pmac import (
     PmacIO,
@@ -27,6 +30,10 @@ from ophyd_async.fastcs.panda import (
     SeqTableInfo,
     SeqTrigger,
     StaticSeqTableTriggerLogic,
+    ScanSpecInfo,
+    ScanSpecSeqTableTriggerLogic,
+    PosOutScaleOffset
+
 )
 from ophyd_async.plan_stubs import ensure_connected
 
@@ -34,7 +41,7 @@ from ophyd_async.plan_stubs import ensure_connected
 # default BL name (from $BEAMLINE)
 BL = get_beamline_name("p99")
 PREFIX = BeamlinePrefix(BL)
-
+PATH = "/dls/p99/data/2025/cm40656-4/tmp/"
 
 @device_factory()
 def panda() -> HDFPanda:
@@ -53,6 +60,19 @@ set_path_provider(
     )
 )
 
+
+def test_store_settings():
+    p = panda()
+    yield from ensure_connected(p)
+    provider = YamlSettingsProvider("./scripts/layouts")
+    yield from store_settings(provider, "seq_table", p)
+
+def test_restore_settings():
+    p = panda()
+    yield from ensure_connected(p)
+    provider = YamlSettingsProvider("./scripts/layouts")
+    settings = yield from retrieve_settings(provider, "seq_table", p)
+    yield from apply_panda_settings(settings)
 
 def no_panda():
     # Defining the frlyers and components of the scan
@@ -121,6 +141,15 @@ def panda_scan(start: float, stop: float, num: int, duration: float):
         else SeqTrigger.POSA_GT
     )
 
+    table += SeqTable.row(
+        repeats=1,
+        trigger=SeqTrigger.BITA_0,
+    )
+    table += SeqTable.row(
+        repeats=1,
+        trigger=SeqTrigger.BITA_1,
+    )
+
     counter = 0
     for pos in positions:
         if counter == num:
@@ -174,3 +203,74 @@ def panda_scan(start: float, stop: float, num: int, duration: float):
         yield from bps.complete_all(pmac_trajectory_flyer, p, panda_seq, wait=True)
 
     yield from inner_plan()
+
+def panda_scan_time_based():
+    p = panda()
+    motor_x = Motor(prefix="BL99P-MO-STAGE-02:X", name="Motor_X")
+    motor_y = Motor(prefix="BL99P-MO-STAGE-02:Y", name="Motor_Y")
+    pmac = PmacIO(
+        prefix="BL99P-MO-STEP-01:",
+        raw_motors=[motor_y, motor_x],
+        coord_nums=[1],
+        name="pmac",
+    )
+    yield from ensure_connected(pmac, motor_x, motor_y, p)
+
+    # Prepare motor info using trajectory scanning
+    spec = Fly(
+        float(1)
+        @ (Line(motor_y, 1, 2, 3) * ~Line(motor_x,1,5,5))
+    )
+
+    trigger_logic = spec
+    pmac_trajectory = PmacTrajectoryTriggerLogic(pmac)
+    pmac_trajectory_flyer = StandardFlyer(pmac_trajectory)
+
+    table = p.seq[1]
+
+    info = ScanSpecInfo(spec=spec, deadtime=0.1)
+    panda_trigger_logic = StandardFlyer(
+        ScanSpecSeqTableTriggerLogic(
+            table,
+            {
+                motor_x: PosOutScaleOffset(
+                    "INENC2.VAL",
+                    p.inenc[2].val_scale,
+                    p.inenc[2].val_offset,
+                ),  # type: ignore
+                motor_y: PosOutScaleOffset(
+                    "INENC3.VAL",
+                    p.inenc[3].val_scale,
+                    p.inenc[3].val_offset,
+                ),  # type: ignore
+            },
+        )
+    )
+    # Prepare Panda file writer trigger info
+    panda_hdf_info = TriggerInfo(
+        number_of_events=3,
+        trigger=DetectorTrigger.CONSTANT_GATE,
+        livetime=1,
+        deadtime=1e-5,
+    )
+
+    @attach_data_session_metadata_decorator()
+    @bpp.run_decorator()
+    @bpp.stage_decorator([p, panda_trigger_logic])
+    def inner_plan():
+        # Prepare pmac with the trajectory
+        yield from bps.prepare(pmac_trajectory_flyer, trigger_logic, wait=True)
+        # prepare sequencer table
+        yield from bps.prepare(panda_trigger_logic, info, wait=True)
+        # prepare panda and hdf writer once, at start of scan
+        yield from bps.prepare(p, panda_hdf_info, wait=True)
+
+        # kickoff devices waiting for all of them
+        yield from bps.kickoff(p, wait=True)
+        yield from bps.kickoff(panda_trigger_logic, wait=True)
+        yield from bps.kickoff(pmac_trajectory_flyer, wait=True)
+
+        yield from bps.complete_all(pmac_trajectory_flyer, p, panda_trigger_logic, wait=True)
+
+    yield from inner_plan()
+
